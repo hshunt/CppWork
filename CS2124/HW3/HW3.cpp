02258416b7fe522c7ec619d921ff12c5c67fabfd
Graeme Ferguson <32684790+gqo@users.noreply.github.com>
2021-11-18 13:15:59 -0500

    Add HW3 comments

--- CS2124/HW3/HW3.cpp
+++ CS2124/HW3/HW3.cpp
@@ -7,7 +7,9 @@ using namespace std;
 class Warrior {
     public:
         Warrior(const string& name, const string& weaponName, int strength) : name(name), weapon(weaponName, strength) {}
-
+        
+        // G: These comments are useless - no getter or setter should need a comment. Additionally, "adjust strength of winning side" doesn't tell me HOW
+        //    that adjustment is made which is the most important part
        // returns name of warrior
         string getName() {
             return name;
@@ -35,7 +37,8 @@ class Warrior {
         class Weapon {
                 public:
                     Weapon(const string& weaponName, int strength) : weaponName(weaponName), strength(strength) {}
-                    // used for getting weapon name public
+                    // G: See comment on comments above in the Warrior class declaration
+                    //    used for getting weapon name public
                     string getWeaponName() {
                         return weaponName;
                     }
@@ -52,11 +55,11 @@ class Warrior {
                         strength = 0;
                     }
                 private:
-                    string weaponName;
+                    string weaponName; // G: This should be const, the weaponName never changes
                     int strength;
             };
 
-        string name;
+        string name; // G: This should be const, the warrior's name never changes
         Weapon weapon;    
 };
 
@@ -70,6 +73,7 @@ int main() {
     ifstream warriorDoc ("warriors.txt");
     
     if (!warriorDoc) {
+        // G: Spelling error in this error message
         cerr << "The file could not be opended!\n";
         exit(1);
     }
@@ -81,6 +85,7 @@ int main() {
     warriorDoc.close();
 }
 
+// G: You did not overload the Weapon operator<< which was stated within the assignment
 // formats output for for class Warrior
 ostream& operator<<(ostream& os, Warrior& warrior) {
     os << "Warrior: " << warrior.getName() << ", weapon: " << 
@@ -120,14 +125,20 @@ void run (ifstream& warriorLine, vector<Warrior>& warriors) {
             }
         }
         else{
+            // G: Spelling error in this error message
             cerr << "Run: Run 'command' not recognized" << endl;
             exit(1);
         }
     }
 }
 
+// G: Both string arguments here should be passed by const-reference
 // takes attributes for Warrior class, creates object, adds to vector of warriors
 void createWarrior (vector<Warrior>& warriors, string name, string weaponName, int strength) {
+    // G: A better version of these two lines is:
+    //    warriors.push_back(Warrior{name, weaponName, strength});
+    // G: Additionally, I commented on this on your previous assignment, please make sure to reference those comments
+    //    when doing your HWs
     Warrior warrior(name, weaponName, strength);
     warriors.push_back(warrior);
 }
@@ -139,6 +150,7 @@ Warrior& selectWarrior (vector<Warrior>& warriors, const string& name) {
             return warrior;
         }
     }
+    // G: Spelling error in this error message
     cerr << "selectWarrior: Did not recognize warrrior name" << endl;
     exit(1);
 }
@@ -152,10 +164,76 @@ Warrior& selectWarrior (vector<Warrior>& warriors, const string& name) {
 void battle (Warrior& offense, Warrior& defense) {
     cout << offense.getName() << " battles " << defense.getName()
         << endl;
+    /* G: Let's think about the number of comparisons necessary within your logic here:
+            For two alive warriors:
+                if (offense dead OR defense dead) => 2 checks
+                total: 2
+            For offense dead, defense alive:
+                if (offense dead OR defense dead) => 1 check (because of short circuiting)
+                if (offense.getStrength() == 0 && defense.getStrength() == 0) => 2 checks
+                else if (offense.getStrength() == 0) => 1 check
+                total: 4
+            For offense alive, defense dead:
+                if (offense dead OR defense dead) => 2 checks
+                if (offense.getStrength() == 0 && defense.getStrength() == 0) => 2 checks
+                else if (offense.getStrength() == 0) => 1 check
+                else => 0 checks
+                total: 5
+            For two dead warriors:
+                if (offense dead OR defense dead) => 1 check
+                if (offense.getStrength() == 0 && defense.getStrength() == 0) => 2 checks
+                total: 3
+                
+                
+            Since we haven't measured how likely any of these scenarios are AND we haven't been told
+            how likely they couple be, we can assume the average number of checks is the total across 
+            these 4 instances (e.g. (2+4+5+3)/4 == 3.5 checks)
+            
+            However, if you structured your logic like this:
+            
+            if (offense.getStrength() == 0 && defense.getStrength() == 0) {
+                ...
+            } else if (offense.getStrength() == 0) {
+                ...
+            } else if (defense.getStrength() == 0) { 
+                ... 
+            } else {
+                // Here you do the battle
+            }
+            
+            For two alive warriors:
+                if (offense.getStrength() == 0 && defense.getStrength() == 0) => 2 checks
+                else if (offense.getStrength() == 0) => 1 check
+                else if (defense.getStrength() == 0) => 1 check
+                else => 0 checks
+                total: 4
+            For offense dead, defense alive:
+                if (offense.getStrength() == 0 && defense.getStrength() == 0) => 2 checks
+                else if (offense.getStrength() == 0) => 1 check
+                total: 3
+            For offense alive, defense dead:
+                if (offense.getStrength() == 0 && defense.getStrength() == 0) => 2 checks
+                else if (offense.getStrength() == 0) => 1 check
+                else if (defense.getStrength() == 0) => 1 check
+                total: 3
+            For two dead warriors:
+                if (offense.getStrength() == 0 && defense.getStrength() == 0) => 2 checks
+                total: 2
+                
+            Following the same math, (4+3+3+2)/4 == 3 which means we've reduced the average
+            checks in your logic by 0.5. I don't expect you to do this math everytime, but
+            my goal here is more to explain my own intuition regarding your control flow 
+            which told me that the OR-statement was superfluous.
+    */
+    
     if (offense.getStrength() == 0 || defense.getStrength() == 0) {
         if (offense.getStrength() == 0 && defense.getStrength() == 0) {
             cout << "Oh, NO! They're both dead! Yuck!\n";
         }
+        // G: This is not behavior specified within the assignment, there is only a special message printed when the
+        //    *target* of the battle is already dead. Dead warriors are allowed to battle alive warriors freely.
+        //    I missed pointing it out as an error in your previous assignment grades though so just be wary of this
+        //    in the future.
         else if (offense.getStrength() == 0) {
             cout << "He's dead, " << defense.getName() << endl;
         }
@@ -182,3 +260,9 @@ void battle (Warrior& offense, Warrior& defense) {
         }
     }
 }
+
+/* G:
+   Good work. One big miss was not overloading the Weapon class's output operator as stated in the HW, otherwise small 
+   errors that did not affect the correctness of your program. Great job with the nested classes. If I were to grade 
+   this, I would give it a B+.
+*/
